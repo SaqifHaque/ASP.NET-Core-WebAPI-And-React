5aff11a391ac9b319a8d657938a2862086c86fd6
Saqif <saqifhaque@gmail.com>
2023-03-02 17:10:15 +0600

    Project Setup

---
+++ RestaurantAPI/Models/FoodItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Models
+{
+    public class FoodItem
+    {
+        [Key]
+        public int FoodItemId { get; set; }
+
+        public string FoodItemName { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
---
+++ RestaurantAPI/Models/OrderMaster.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Models
+{
+    public class OrderMaster
+    {
+        [Key]
+        public long OrderMasterId { get; set; }
+
+        public string OrderNumber { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public string PMethod { get; set; }
+
+        public int MyProperty { get; set; }
+
+        public decimal GTotal { get; set; }
+
+
+    }
+}
