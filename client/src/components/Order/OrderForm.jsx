1055e894bea53f1815d68189cf498ecca0b51c2c
Saqif <saqifhaque@gmail.com>
2023-03-16 21:44:01 +0600

    Working on input field design

--- client/src/components/Order/OrderForm.jsx
+++ client/src/components/Order/OrderForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import Form from "../../layouts/Form";
-import { Grid  } from "@material-ui/core";
+import { ButtonGroup, Grid, InputAdornment, makeStyles, Button as MuiButton } from "@material-ui/core";
 import Input from "../../controls/Input";
 import Select from "../../controls/Select";
 
@@ -10,18 +10,43 @@ const pMethods = [
   {id: 'Card', title: 'Card'},
 ]
 
+const useStyles = makeStyles(theme =>  ({
+  adornmentText: {
+    '& .MuiTypography-root': {
+      color: '#f3b33d',
+      fontWeight: 'border',
+      fontSize: '1.5em'
+    }
+  }
+}))
+
 const OrderForm = (props) => {
   const {values, errors, handleInputChange } = props;
+  const classes = useStyles();
+  
   return (
     <Form>
       <Grid container>
         <Grid item xs={6}>
-            <Input disabled value={values.orderNumber}  onChange={handleInputChange} label="Order number" name="orderNumber"/>
+            <Input disabled value={values.orderNumber}  onChange={handleInputChange} label="Order number"
+             InputProps={{
+              startAdornment: <InputAdornment 
+              position="start"
+              className={classes.adornmentText}>#</InputAdornment>
+            }} name="orderNumber"/>
             <Select label="Customer" onChange={handleInputChange} value={values.customerId} name="customerId" options={[]} />
         </Grid>
         <Grid item xs={6}>
           <Select label="Payment Method" value={values.pMethod}  onChange={handleInputChange} name="pMethod" options={pMethods} />
-          <Input disabled value={values.gTotal}  onChange={handleInputChange} label="Grand Total" name="gTotal"/>
+          <Input disabled value={values.gTotal}  onChange={handleInputChange} label="Grand Total"
+           InputProps={{
+            startAdornment: <InputAdornment
+            position="start"
+            className={classes.adornmentText}>$</InputAdornment>
+          }} name="gTotal"/>
+          <ButtonGroup>
+              <MuiButton size="large"/>
+          </ButtonGroup>
         </Grid>
       </Grid>
     </Form>
