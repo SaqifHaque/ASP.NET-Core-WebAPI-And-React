68f9cdfb213de75542b833f52b92d0cfee72c3b8
Saqif <saqifhaque@gmail.com>
2023-03-17 21:13:19 +0600

    cors configuration

--- RestaurantAPI/Startup.cs
+++ RestaurantAPI/Startup.cs
@@ -35,6 +35,10 @@ namespace RestaurantAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(options => options.WithOrigins("http://localhost:3000")
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
