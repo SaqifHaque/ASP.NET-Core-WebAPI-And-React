3ca5db4095459d0ee70961cfe25b11630ff8ec41
Saqif <saqifhaque@gmail.com>
2023-03-31 22:47:02 +0600

    working on search configuration and ui

--- client/src/components/Order/SearchFoodItems.jsx
+++ client/src/components/Order/SearchFoodItems.jsx
@@ -1,16 +1,77 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { createApiEndpoint, ENDPOINTS } from '../../api'
+import { IconButton, InputBase, List, ListItem, ListItemSecondaryAction, ListItemText, makeStyles, Paper } from '@material-ui/core';
+import SearchTwoToneIcon from 'material-ui/icons/SearchTwoToneIcon';
+import PlusOneIcon from 'material-ui/icons/PlusOneIcon';
+import ArrowForwardIosIcon from 'material-ui/icons/ArrowForwardIosIcon';
+
+
+const useStyles = makeStyles (theme => ({
+  searchPaper: {
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center'
+  },
+  searchInput: {
+    marginLeft: theme.spacing(1.5),
+    flex: 1
+  },
+  listRoot: {
+    
+  }
+}))
 
 const SearchFoodItems = () => {
 
+    const [foodItems, setFoodItems] = useState([]);
+    const [searchList, setSearchList] = useState([]);
+    const [searchKey, setSearchKey] = useState("");
+    const classes = useStyles();  
+
     useEffect(() => {
         createApiEndpoint(ENDPOINTS.FOODITEM).fetchAll()
-        .then(() => {})
+        .then((res) => {
+          setFoodItems(res.data);
+          setSearchList(res.data);
+        })
         .catch((err) => console.log(err));
     }, [])
+
+    useEffect(() => {
+      let search = [...foodItems];
+      search = search.filter(y => {
+        return y.foodItemName.toLowerCase().includes(searchKey.toLowerCase());
+      });
+      setSearchList(search);
+
+    }, [searchKey])
+    
     
   return (
-    <div>SearchFoodItems</div>
+    <>
+    <Paper className={classes.searchPaper}>
+        <InputBase className={classes.searchInput} value={searchKey}
+        onChange={ e => setSearchKey(e.target.value)} placeholder="Search food Items"/>
+        <IconButton>
+          <SearchTwoToneIcon />
+        </IconButton>
+    </Paper>
+    <List>
+      {
+        searchList.map((items, index) => (
+          <ListItem key={index}>
+            <ListItemText primary={items.foodItemName} secondary= {'$' + items.price}/>
+            <ListItemSecondaryAction>
+              <IconButton>
+                  <PlusOneIcon/>
+                  <ArrowForwardIosIcon/>
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+        ))
+      }
+    </List>
+    </>
   )
 }
 
